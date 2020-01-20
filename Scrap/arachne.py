3d870effe043a7d57795699d898482e72432aa84
Luiz Augusto Machado <luizmachado.to@gmail.com>
2020-01-20 14:59:51 -0300

    Trying to get another type of file

--- Scrap/arachne.py
+++ Scrap/arachne.py
@@ -28,13 +28,12 @@ def cadastra():
 
 ### Função minera
 def minera(ativos_lista = ["XPML11", "XPIN11", "IRDM11"]):
-    urlpage1 = "https://www.clubefii.com.br/fiis/" + i
     driver = webdriver.Firefox()
     cotacao = []
     for i in ativos_lista:
         #Site 1
+        urlpage1 = "https://www.clubefii.com.br/fiis/" + i
         driver.get(urlpage1)
-        time.sleep(5)
         title1 = driver.title
         window1 = driver.current_window_handle
         results = driver.find_element_by_xpath\
@@ -43,12 +42,21 @@ def minera(ativos_lista = ["XPML11", "XPIN11", "IRDM11"]):
     driver.quit()
 
     return(info1, window1)
+### Função furtiva (Use com responsabilidade! Uso didático)    
+def furtiva(url='https://www.brandcrowd.com/maker/logo/ce07c4ab-a95a-4efc-8872-6267683f4c50/draft/4c65c7c7-076b-4505-a790-900e7f546c14'):
+    driver = webdriver.Firefox()
+    driver.get(url)
+    results = driver.find_elements_by_class_name('maker-canvas-container')
+    imagem = results.get_attribute("value")
+    driver.quit()
 
-ativos_lista = cadastra()
+menu = input("Função Minera ou Furtiva.(M ou F):\n")
+if menu.capitalize == 'M':
+    ativos_lista = cadastra()
 
-if ativos_lista:
-    info_ativos = minera(ativos_lista)
+    if ativos_lista:
+        info_ativos = minera(ativos_lista)
+    else:
+        info_ativos = minera()
 else:
-    info_ativos = minera()
-
-print(info_ativos[0])
+    furtiva()
