2ba183a3f854e01953e22642cbac8137e867d65b
Luiz Augusto Machado <luizmachado.to@gmail.com>
2020-01-31 11:17:33 -0300

    Sort files by extension

--- KalleHallden/moving_files.py
+++ KalleHallden/moving_files.py
@@ -2,19 +2,22 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 import os
-#import json
+import re
 
 class MyHandler(FileSystemEventHandler):
     i = 1
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
-            src = folder_to_track + '/' + filename
-            new_destination = folder_destination + '/' + filename
-            os.rename(src, new_destination)
+            src = folder_to_track + "/" + filename
+            #if src != folder_destination and re.search('^.*\.(avi|webm)$', filename) is not None:
+            if re.search('^.*\.(avi|webm)$', filename) is not None:
+                new_destination = filme_destination + "/" + filename
+                os.rename(src, new_destination)
 
 
 folder_to_track = '/home/luiz/Transferências'
 folder_destination = '/home/luiz/Transferências/Arquivo'
+filme_destination = '/home/luiz/Transferências/Arquivo/Videos'
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
